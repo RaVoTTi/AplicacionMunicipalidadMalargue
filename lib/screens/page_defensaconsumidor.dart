d44888764c88eff73fc9400bb76d4868a8d6a463
unknown <alohadesign23>
2021-07-21 09:53:51 -0300

    Actualizacion 21/07/2021 Defensa Consumidor

--- lib/screens/page_defensaconsumidor.dart
+++ lib/screens/page_defensaconsumidor.dart
@@ -9,9 +9,11 @@ import 'package:municipalidad_de_malargue/screens/page_servicios.dart';
 import 'package:municipalidad_de_malargue/widgets/widgets.dart';
 
 Map<String, String> _urls = {
-  'formdefensaconsumidor': 'https://docs.google.com/forms/d/1scsWmVjHVUKyKyLHTgpdzFhoVmtb0b-CMeAAI0BHcV8/edit?usp=sharing',
+  'formdefensaconsumidor':
+      'https://docs.google.com/forms/d/1scsWmVjHVUKyKyLHTgpdzFhoVmtb0b-CMeAAI0BHcV8/edit?usp=sharing',
   'teldefensaconsumidor': '+5492604059263',
-  'abrirurldefensa': 'https://www.mendoza.gov.ar/prensa/defensa-del-consumidor-las-oficinas-municipales-prestan-un-servicio-esencial-para-los-ciudadanos/',
+  'abrirurldefensa':
+      'https://www.mendoza.gov.ar/prensa/defensa-del-consumidor-las-oficinas-municipales-prestan-un-servicio-esencial-para-los-ciudadanos/',
 };
 
 class DefensaConsumidorPage extends StatelessWidget {
@@ -23,93 +25,92 @@ class DefensaConsumidorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final iconSize = height * 0.05;
-    return Padding(
-      padding: const EdgeInsets.all(5.0),
-      child: ListView(
-        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          // Slider Principal//
-          CarouselSlider(
-            options: CarouselOptions(
-              height: this.height * 0.2,
-              aspectRatio: 16 / 9,
-              viewportFraction: 0.9,
-              enableInfiniteScroll: false,
-              autoPlay: true,
-              autoPlayInterval: Duration(seconds: 3),
-              autoPlayAnimationDuration: Duration(milliseconds: 800),
-              autoPlayCurve: Curves.fastOutSlowIn,
-            ),
-            items: [
-              //AGREGAR IMAGENES AQUI DE DEFENSA AL CONSUMIDOR
-              CardCarousel(
-                function: () => openLink(_urls['abrirurldefensa']),
-                urlImage:
-                    'https://www.malargue.gov.ar/wp-content/uploads/2021/07/fbe5702e-77b4-4d9d-936f-c99da4f3fc62.jpg',
-              ),
-              CardCarousel(
-                function: () => openLink(_urls['abrirurldefensa']),
-                urlImage:
-                    'https://www.malargue.gov.ar/wp-content/uploads/2021/07/54ec35c0-0510-499a-b314-214196abd4c5.jpg',
-              ),
-              CardCarousel(
-                function: () => openLink(_urls['abrirurldefensa']),
-                urlImage:
-                    'https://www.malargue.gov.ar/wp-content/uploads/2021/07/b318c86f-d680-4e2a-877d-8eb323aebd43.jpg',
-              ),
-              CardCarousel(
-                function: () => openLink(_urls['abrirurldefensa']),
-                urlImage:
-                    'https://www.malargue.gov.ar/wp-content/uploads/2021/07/bb9a2a71-c2f6-470b-aef9-5359105b1415.jpg',
-              ),
-              CardCarousel(
-                function: () => openLink(_urls['abrirurldefensa']),
-                urlImage:
-                    'https://www.malargue.gov.ar/wp-content/uploads/2021/07/cf70ee4a-bd29-424b-b1a1-9ec1eae543ea.jpg',
-              ),
-            ],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'Defensa Consumidor',
+          style: TextStyle(
+            color: Color(0xff000939d),
           ),
-          SizedBox(
-            height: height * 0.01,
+        ),
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Color(0xff00939d),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              BtnGenerico(
-                function: () => openLink(_urls['formdefensaconsumidor']),
-                name: ("Registrar denuncia"),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: ListView(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            // Slider Principal//
+            CarouselSlider(
+              options: CarouselOptions(
+                height: this.height * 0.2,
+                aspectRatio: 16 / 9,
+                viewportFraction: 0.9,
+                enableInfiniteScroll: false,
+                autoPlay: true,
+                autoPlayInterval: Duration(seconds: 3),
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
               ),
-              BtnGenerico(
-                function: () => openLink(_urls['teldefensaconsumidor']),
-                name: ("Llamar"),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: height * 0.01,
-          ),
-          CarouselSlider(
-            options: CarouselOptions(
-              height: this.height * 0.28,
-              aspectRatio: 16 / 9,
-              viewportFraction: 0.90,
-              enableInfiniteScroll: false,
-              autoPlay: false,
-              autoPlayInterval: Duration(seconds: 3),
-              autoPlayAnimationDuration: Duration(milliseconds: 800),
-              autoPlayCurve: Curves.fastOutSlowIn,
-            ),
-            items: [
-              CardCarousel(
-                  function: () => openLink(_urls['chatBot']),
+              items: [
+                //AGREGAR IMAGENES AQUI DE DEFENSA AL CONSUMIDOR
+                CardCarousel(
+                  function: () => openLink(_urls['abrirurldefensa']),
                   urlImage:
-                      'https://www.malargue.gov.ar/wp-content/uploads/2021/04/ezgif.com-gif-maker-1.gif'),
-              // fit: BoxFit.cover,),
-            ],
-          ),
-        ],
+                      'https://www.malargue.gov.ar/wp-content/uploads/2021/07/fbe5702e-77b4-4d9d-936f-c99da4f3fc62.jpg',
+                ),
+                CardCarousel(
+                  function: () => openLink(_urls['abrirurldefensa']),
+                  urlImage:
+                      'https://www.malargue.gov.ar/wp-content/uploads/2021/07/54ec35c0-0510-499a-b314-214196abd4c5.jpg',
+                ),
+                CardCarousel(
+                  function: () => openLink(_urls['abrirurldefensa']),
+                  urlImage:
+                      'https://www.malargue.gov.ar/wp-content/uploads/2021/07/b318c86f-d680-4e2a-877d-8eb323aebd43.jpg',
+                ),
+                CardCarousel(
+                  function: () => openLink(_urls['abrirurldefensa']),
+                  urlImage:
+                      'https://www.malargue.gov.ar/wp-content/uploads/2021/07/bb9a2a71-c2f6-470b-aef9-5359105b1415.jpg',
+                ),
+                CardCarousel(
+                  function: () => openLink(_urls['abrirurldefensa']),
+                  urlImage:
+                      'https://www.malargue.gov.ar/wp-content/uploads/2021/07/cf70ee4a-bd29-424b-b1a1-9ec1eae543ea.jpg',
+                ),
+              ],
+            ),
+            SizedBox(
+              height: height * 0.01,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                BtnGenerico(
+                  function: () => openLink(_urls['formdefensaconsumidor']),
+                  name: ("Registrar denuncia"),
+                ),
+                BtnGenerico(
+                  function: () => openLink(_urls['teldefensaconsumidor']),
+                  name: ("Llamar"),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: height * 0.01,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
